104cebd1f29afd4be2c8b628392b25738858c278
danny-ruiz-cloud <dannyleematthew@gmail.com>
2026-03-11 00:45:31 -0400

    robinhood sql interview questions - cleaned up

--- sql projects/robinhood_trades.sql
+++ sql projects/robinhood_trades.sql
@@ -1,4 +1,30 @@
+
 /*
+  Prompt: 
+    Write a query to retrieve the top three cities 
+    that have the highest number of completed trade orders listed in descending order. 
+    Output the city name and the corresponding number of completed trade orders.
+
+    get(): 
+      top 3 cities { highest number of completed trades in DESC }
+    output:
+      city_name
+      completed_trade_count
+*/
+
+SELECT 
+  city, 
+  COUNT(city) AS city_count -- Counts the amount of entries of said city
+FROM trades t
+JOIN users u
+  ON t.user_id = u.user_id
+WHERE status = 'Completed'
+GROUP BY city
+ORDER BY city_count DESC LIMIT 3
+
+
+/*
+This script sums up all quantities to find the city with the highest quantity of completed trades
 SELECT 
   city,
   SUM(t.quantity) AS completed_trade_count
@@ -10,26 +36,3 @@ WHERE status = 'Completed'
 GROUP BY city
 ORDER BY completed_trade_count DESC LIMIT 3;
 */
-/*
-  get(): 
-    top 3 cities { highest number of completed trades in DESC }
-  output:
-    city_name
-    completed_trade_count
-    
-    
-SELECT DISTINCT ON (status)
-  order_id,
-  status,
-  user_id
-*/
-
-SELECT 
-  city, 
-  COUNT(city) AS city_count
-FROM trades t
-JOIN users u
-  ON t.user_id = u.user_id
-WHERE status = 'Completed'
-GROUP BY city
-ORDER BY city_count DESC LIMIT 3
